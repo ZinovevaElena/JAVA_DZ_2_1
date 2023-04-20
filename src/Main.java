fbc3c08abb79fa18fa27ae2ea76bd2147827197f
Laco test <lo25654@yandex.ru>
2023-04-20 12:50:53 +0300

    New commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 28207; // стоимость билета
+        int miles = ticketprice / 20; // за каждые 20 рублей 1 миля
+        System.out.println("При покупке билета будет начислено");
+        System.out.println(miles + "  милей ");
+
+    }
+}
